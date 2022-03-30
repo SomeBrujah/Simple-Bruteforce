2cd498a356202d952d220849fe1179d04840cedd
SomeBrujah <wissenseeker@gmail.com>
2022-03-30 17:54:18 +0300

    2

--- recursion.js
+++ recursion.js
@@ -7,16 +7,5 @@ function login(password) {
 function brute(maxLength=5) {
 
 }
-function allPossibleCombinations(arr, length, letterComb) {
-    if(letterComb.length >= length) {
-      return [];                
-    }
-    let resultArray = [];
-    for(var i = 0; i < arr.length; i++) {
-      resultArray.push(letterComb + arr[i]);
-      resultArray = resultArray.concat(allPossibleCombinations(arr, length, letterComb + arr[i]));
-    }
-    return resultArray;
-  }
-  var array = ['a', 'b', 'c', 'A', 'B', 'C'];
-  console.log(allPossibleCombinations(array, 2, ''));
+
+

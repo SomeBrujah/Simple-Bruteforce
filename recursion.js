7fff1380b70a954982485fbc1152d3f39c80403d
SomeBrujah <wissenseeker@gmail.com>
2022-03-30 19:28:33 +0300

    recursion version

--- recursion.js
+++ recursion.js
@@ -4,8 +4,34 @@ function login(password) {
   return password === 'AcBcC'
 }
 
-function brute(maxLength=5) {
+function brute(maxLength = 5) {
+ 
+ for (let i = 1; i <= maxLength; i++) {
+  let combinations = genString(allowChars, i);
+  for (let i = 0; i < combinations.length; i++) {
+    if(login(combinations[i])) return combinations[i];
+  }
+ }
 
+ return null;
 }
 
 
+function genString(charArr, length, stringVariant = '') {
+
+  if (stringVariant.length >= length) return new Array();
+
+  let collection = new Array();
+
+  for(let i = 0; i < charArr.length; i++) {
+    let word = stringVariant+ charArr[i];
+    collection.push(word);
+    collection = collection.concat(genString(charArr, length, stringVariant + charArr[i]));
+  }
+
+  return collection;
+}
+
+// console.log(genString(allowChars, 2));
+
+console.log(brute());

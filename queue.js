e3a9f989917c3adbc4c91775d2e0effa6a530e45
SomeBrujah <wissenseeker@gmail.com>
2022-04-01 21:42:16 +0300

    queue

--- queue.js
+++ queue.js
@@ -1,4 +1,4 @@
-// Create task emulation
+// Создаём функцию 
 function task() {
     return new Promise(function (resolve, reject) {
         setTimeout(() => {
@@ -9,29 +9,38 @@ function task() {
 }
 
 // Create array with task
-const tasksArr = new Array(10).fill(task);
+const tasksArr = new Array(30).fill(task);
 
 // Create our class
 class Queue {
     constructor(competition, tasks) {
         this.tasks = tasks;
         this.competition = competition;
-        this.currQueue = 0;
     }
 
+    // Отдаём на выполнение столько промисов, сколько указано очередью
     start() {
-        while (this.tasks.length > 0) {
-            
+        for(let i = 0; i < this.competition; i++) {
+            this.add(this.tasks.pop())
         }
+        console.log(`!${this.competition} promises in queue.!`)
     }
 
+    // Вспомогательная функция для отдачи промиса на выполнение
     add(task) {
         this.execute(task);
     }
 
+    // Каждый из тех промисов который был отдан на выполнение
+    // после его выполнения проверяет если задачи ещё есть - то отправляет на выполнение следующую задачу
+    // Таким образом сохраняется очередь в заданое кол-во промисов так как каждый следующий вызов делает
+    // переданный промис ЛИШЬ ПОСЛЕ СВОЕГО ВЫПОЛНЕНИЯ
     execute(task) {
         task().then((result) => {
             console.log(result);
+            if(this.tasks.length > 0) {
+                this.add(this.tasks.pop())
+            }
         })
     }
 }
